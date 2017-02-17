a5a98683e2989c46c6a093517a9be23fde50184b
jzeferino <jorgevalentzeferino@gmail.com>
2017-02-17 14:41:02 +0000

    Refactored the code to support multi cultures and more extensibility.

--- ResxParser/Program.cs
+++ ResxParser/Program.cs
@@ -1,51 +1,12 @@
-﻿using System;
-using System.IO;
-using System.Xml;
-
-namespace ResxParser
+﻿namespace ResxParser
 {
     public class MainClass
     {
         public static void Main(string[] args)
         {
-            var outputFile = "example.xml";
-            using (var file = new StreamWriter(outputFile))
-            {
-                ParseResx((arg) => file, (arg) => $"{arg.Key}={arg.Value}", (arg) => $"/*{arg}*/");
-            }
-        }
-
-        public static void ParseResx(Func<string, StreamWriter> createExternalFile, Func<StringElement, string> writeString, Func<string, string> writeComment)
-        {
-            foreach (string file in Directory.EnumerateFiles("Resources", "*.resx"))
-            {
-                var externalFile = createExternalFile(Path.GetFileName(file));
-
-                var doc = new XmlDocument();
-                doc.Load(file);
-
-                foreach (XmlNode node in doc.DocumentElement)
-                {
-                    if (node.NodeType == XmlNodeType.Element && node.Name == "data")
-                    {
-                        externalFile.WriteLine(writeString(new StringElement
-                        {
-                            Key = node.Attributes.GetNamedItem("name").Value,
-                            Value = node.InnerText
-                        }));
-                    }
-                    else if (node.NodeType == XmlNodeType.Comment)
-                    {
-                        externalFile.WriteLine(writeComment(node.Value));
-                    }
-                }
-            }
-        }
-
-        public class StringElement
-        {
-            public string Key { get; set; }
-            public string Value { get; set; }
+            var resxFolder = "Resources";
+            var outputFile = "res";
+            ResxConverter.Instance.ConvertToAndroid(resxFolder, outputFile);
         }
     }
 }
---
+++ ResxParser/ResxConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ResxParser
+{
+    public sealed class ResxConverter
+    {
+        private static readonly Lazy<ResxConverter> lazy =
+            new Lazy<ResxConverter>(() => new ResxConverter());
+
+        public static ResxConverter Instance { get { return lazy.Value; } }
+
+        private ResxConverter()
+        {
+        }
+
+        public void Convert(string folder, Func<string, IResxConverterOutput> outputFactory)
+        {
+            foreach (string file in Directory.EnumerateFiles(folder, "*.resx"))
+            {
+                using (var outout = outputFactory(file))
+                {
+                    var doc = XDocument.Load(file);
+
+                    foreach (var element in doc.Descendants())
+                    {
+                        if (element.NodeType == XmlNodeType.Element && element.Name == "data")
+                        {
+                            outout.WriteString((new StringElement
+                            {
+                                Key = element.FirstAttribute.Value,
+                                Value = element.Value
+                            }));
+                        }
+                        else if (element.NodeType == XmlNodeType.Comment)
+                        {
+                            outout.WriteComment(element.Value);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
