e0c328780a8cea5ff6028d134b166d68ca63a230
Luís Gonçalves <luis.fgoncalv@gmail.com>
2017-03-09 22:32:13 +0000

    Add complete usage samples to unit tests project.

---
+++ test/ResxConverter.Mobile.Tests/UsageSamples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace ResxConverter.Mobile.Tests
+{
+    public class UsageSamples : IDisposable
+    {
+        private readonly DirectoryInfo _rootFolder, _androidFolder, _iOSFolder;
+
+        public UsageSamples()
+        {
+            _rootFolder = new DirectoryInfo(Guid.NewGuid().ToString());
+            _androidFolder = _rootFolder.CreateSubdirectory("android");
+            _iOSFolder = _rootFolder.CreateSubdirectory("ios");
+        }
+
+        public void Dispose()
+        {
+            _rootFolder.Delete(true);
+        }
+
+        [Fact]
+        public void Run()
+        {
+            ResxConverters.Android.Convert("Resources", _androidFolder.FullName);
+            ResxConverters.iOS.Convert("Resources", _iOSFolder.FullName);
+        }
+    }
+}
