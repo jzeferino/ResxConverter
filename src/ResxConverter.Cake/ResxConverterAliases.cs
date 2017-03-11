da5111b7fd0b0f2ae54ccc9f17ee5412c1e760a4
Luís Gonçalves <luis.fgoncalv@gmail.com>
2017-03-11 17:56:05 +0000

    Cake add-in: add XML docs and usage example (#18)

--- src/ResxConverter.Cake/ResxConverterAliases.cs
+++ src/ResxConverter.Cake/ResxConverterAliases.cs
@@ -10,6 +10,23 @@ namespace ResxConverter.Cake
     [CakeAliasCategory("ResxConverter")]
     public static class ResxConverterAliases
     {
+        /// <summary>
+        /// ResxConverter alias.
+        /// </summary>
+        /// <example>
+        /// <para>Cake task:</para>
+        /// <code>
+        /// <![CDATA[
+        /// Task("ResxConverter")
+        /// .Does(() =>
+        /// {
+        ///     ResxConverter.ConvertToAndroid("./Resources", "./Generated");
+        /// });
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <param name="context">The Cake context</param>
+        /// <returns>ResxCoverter conversion options</returns>
         [CakePropertyAlias(Cache = true)]
         public static ResxConverterProvider ResxConverter(this ICakeContext context)
         {
--- src/ResxConverter.Cake/ResxConverterProvider.cs
+++ src/ResxConverter.Cake/ResxConverterProvider.cs
@@ -2,9 +2,23 @@
 
 namespace ResxConverter.Cake
 {
+    /// <summary>
+    /// Provides different conversion methods.
+    /// </summary>
     public class ResxConverterProvider
     {
+        /// <summary>
+        /// Converts Resx files to Android <code>string.xml</code> files.
+        /// </summary>
+        /// <param name="inputFolder">The input folder for Resx files</param>
+        /// <param name="outputFolder">The output folder for string files</param>
         public void ConvertToAndroid(string inputFolder, string outputFolder) => ResxConverters.Android.Convert(inputFolder, outputFolder);
+
+        /// <summary>
+        /// Converts Resx files to iOS <code>Localizable.strings</code> files.
+        /// </summary>
+        /// <param name="inputFolder">The input folder for Resx files</param>
+        /// <param name="outputFolder">The output folder</param>
         public void ConvertToiOS(string inputFolder, string outputFolder) => ResxConverters.iOS.Convert(inputFolder, outputFolder);
     }
 }
