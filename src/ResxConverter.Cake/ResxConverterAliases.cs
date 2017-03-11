047485936b3cbe07d1c261fed5e8a41ae6aa805b
Luís Gonçalves <luis.fgoncalv@gmail.com>
2017-03-11 15:22:38 +0000

    Add first version of cake addin (#18)

---
+++ src/ResxConverter.Cake/ResxConverterAliases.cs
@@ -0,0 +1,24 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace ResxConverter.Cake
+{
+    /// <summary>
+    /// Contains alias for converting .NET Resx files to different output formats.
+    /// </summary>
+    [CakeAliasCategory("ResxConverter")]
+    public static class ResxConverterAliases
+    {
+        [CakePropertyAlias(Cache = true)]
+        public static ResxConverterProvider ResxConverter(this ICakeContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return new ResxConverterProvider();
+        }
+    }
+}
---
+++ src/ResxConverter.Cake/ResxConverterProvider.cs
@@ -0,0 +1,10 @@
+﻿using ResxConverter.Mobile;
+
+namespace ResxConverter.Cake
+{
+    public class ResxConverterProvider
+    {
+        public void ConvertToAndroid(string inputFolder, string outputFolder) => ResxConverters.Android.Convert(inputFolder, outputFolder);
+        public void ConvertToiOS(string inputFolder, string outputFolder) => ResxConverters.iOS.Convert(inputFolder, outputFolder);
+    }
+}
